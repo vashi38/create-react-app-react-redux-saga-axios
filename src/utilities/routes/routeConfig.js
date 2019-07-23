c72128395ad28f67485d8e71150976dec2a00e38
Shivanand Sonnad <shivanand.sonnad@knorex.com>
2019-07-23 19:06:20 +0530

    add routes array prop in route component.

--- src/utilities/routes/routeConfig.js
+++ src/utilities/routes/routeConfig.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 function ReactRouteConfig({ routes, store }) {
-    function renderRoutes(routes, path) {
+    function getPath(item, path) {
+        return `${path}/${item.path}`.replace(/\/\//g, '/');
+    }
+    function renderRoutes(routes, path, routesArray) {
         return (
             <div>
                 <Switch>
                     {routes.map((item, index) => (
                         <Route
                           key={index} 
-                          path={`${path}/${item.path}`.replace(/\/\//g, '/')} 
+                          path={getPath(item, path)} 
                           exact={!!item.exact}
                           render={
                               (props) => {
@@ -17,14 +20,16 @@ function ReactRouteConfig({ routes, store }) {
                                   if (typeof item.onEnter === 'function') {
                                       item.onEnter(store);
                                   }
+                                  console.log(routesArray);
+                                  routesArray.push(item);
                                   if(item.childRoutes) {
                                       return (
-                                          <item.component {...props} route={item} store={store} >
-                                              {renderRoutes(item.childRoutes, match.path)}
+                                          <item.component {...props} route={item} store={store} routesArray={routesArray} >
+                                              {renderRoutes(item.childRoutes, match.path, routesArray)}
                                           </item.component>
                                       );
                                   }
-                                  return <item.component {...props} route={item} store={store} />
+                                  return <item.component {...props} route={item} store={store} routesArray={routesArray} />
                               }
                           }
                         > 
@@ -34,7 +39,7 @@ function ReactRouteConfig({ routes, store }) {
             </div>
         );
     }
-    return renderRoutes(routes, '/');
+    return renderRoutes(routes, '/', []);
 }
 
 export default ReactRouteConfig;
