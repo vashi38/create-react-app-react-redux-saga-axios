0cb2f5d579defa077227372dc345d323999ae1d3
Shivanand Sonnad <shivanand.sonnad@knorex.com>
2019-08-04 17:57:26 +0530

    some bugfixes

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 // const functions = require('firebase-functions');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db1.json');
+const router = jsonServer.router('db.json');
+// const router = jsonServer.router('db1.json');
 const middleware = jsonServer.defaults();
 const cors = require('cors')({origin: true});
 
